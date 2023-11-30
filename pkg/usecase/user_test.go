02c37aa7b562ce07d17d1024d7946d6df37fdcdb
sgokul961 <sgokul961@gmail.com>
2023-11-30 21:39:56 +0530

    ssh added

--- pkg/usecase/user_test.go
+++ pkg/usecase/user_test.go
@@ -1,233 +1,16 @@
 package usecase
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 
+	"github.com/go-playground/assert/v2"
 	"github.com/golang/mock/gomock"
 	"gokul.go/pkg/config"
 	"gokul.go/pkg/mock"
 	"gokul.go/pkg/utils/models"
 )
-
-// func Test_User_UserSignUp(t *testing.T) {
-
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
-// 	// Create mock implementations for the repositories
-
-// 	userRepo := mock.NewMockUserRepository(ctrl)
-// 	orderRepo := mock.NewMockOrderRepository(ctrl)
-// 	otpRepo := mock.NewMockOtpRepository(ctrl)
-// 	helper := mock.NewMockHelper(ctrl)
-// 	cfg := config.Config{}
-
-// 	type args struct {
-// 		user models.UserDetails
-// 		ref  string
-// 	}
-
-// 	userUseCase := NewUserUseCase(userRepo, cfg, otpRepo, helper, orderRepo)
-// 	testData := map[string]struct {
-// 		input          args
-// 		StubDetails    func(mock.MockUserRepository, mock.MockHelper, args)
-// 		expectedOutput models.TokenUsers
-// 		expectedError  error
-// 	}{
-// 		"success": {
-// 			input: args{user: models.UserDetails{
-// 				Name:            "gokul",
-// 				Email:           "gokul@gmail.com",
-// 				Phone:           "6282288363",
-// 				Password:        "hffj",
-// 				ConfirmPassWord: "hffj",
-// 			}, ref: "gok",
-// 			},
-
-// 			StubDetails: func(userRepo mock.MockUserRepository, helper mock.MockHelper, signupData args) {
-// 				gomock.InOrder(
-// 					userRepo.EXPECT().CheckUserAvailability(signupData.user.Email).Times(1).Return(false),
-// 					userRepo.EXPECT().FindUserFromReference(signupData.ref).Times(1).Return(1, nil),
-// 					helper.EXPECT().GenerateRefferalCode().Times(1).Return("goku", nil),
-// 					userRepo.EXPECT().UserSignUp(signupData.user, "goku").Times(1).Return(
-// 						func(user models.UserDetails, referral string) models.UserDeatilsResponse {
-// 							// Simulate password hashing before storing in the database
-// 							hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-// 							return models.UserDeatilsResponse{
-// 								Id:    2,
-// 								Name:  user.Name,
-// 								Email: user.Email,
-// 								Phone: user.Phone,
-// 							}
-// 						},
-// 						models.UserDeatilsResponse{
-// 							Id:    2,
-// 							Name:  signupData.user.Name,
-// 							Email: signupData.user.Email,
-// 							Phone: signupData.user.Phone,
-// 						}, nil),
-// 					helper.EXPECT().GenerateTokenClients(models.UserDeatilsResponse{
-// 						Id:    2,
-// 						Name:  signupData.user.Name,
-// 						Email: signupData.user.Email,
-// 						Phone: signupData.user.Phone,
-// 					}).Times(1).Return("ajjsjsjsjsjs.sjsjsjsjsjs.sjsjsjsjs", nil),
-// 					userRepo.EXPECT().CreditReferencePointsToWallet(1).Times(1).Return(nil),
-// 					orderRepo.EXPECT().CreateNewWallet(2).Times(1).Return(1, nil),
-// 				)
-// 			},
-
-// 			expectedOutput: models.TokenUsers{
-// 				Users: models.UserDeatilsResponse{
-// 					Id:    2,
-// 					Name:  "gokul",
-// 					Email: "gokul@gmail.com",
-// 					Phone: "6282288363",
-// 				},
-// 				Token: "ajjsjsjsjsjs.sjsjsjsjsjs.sjsjsjsjs",
-// 			},
-// 			expectedError: nil,
-// 		},
-// 	}
-// 	for _, test := range testData {
-// 		test.StubDetails(*userRepo, *helper, test.input)
-
-// 		tokenusers, err := userUseCase.UserSignUp(test.input.user, test.input.ref)
-// 		assert.Equal(t, test.expectedOutput.Users.Id, tokenusers.Users.Id)
-// 		assert.Equal(t, test.expectedOutput.Users.Name, tokenusers.Users.Name)
-// 		assert.Equal(t, test.expectedOutput.Users.Email, tokenusers.Users.Email)
-// 		assert.Equal(t, test.expectedError, err)
-
-// 	}
-
-// }
-
-// func Test_User_LoginHandler(t *testing.T) {
-// 	cntrl := gomock.NewController(t)
-// 	// Create mock implementations for the repositories
-
-// 	userRepo := mocks.NewMockUserRepository(cntrl)
-// 	helper := mocks.NewMockHelper(cntrl)
-// 	cfg := config.Config{}
-
-// 	type args struct {
-// 		user models.UserLoign
-// 	}
-
-// 	userUseCase := NewUserUseCase(userRepo, cfg, nil, nil, helper)
-// 	testData := map[string]struct {
-// 		input          args
-// 		StubDetails    func(mocks.MockUserRepository, mocks.MockHelper, args)
-// 		expectedOutput models.TokenUsers
-// 		expectedError  error
-// 	}{
-// 		"success": {
-// 			input: args{
-// 				user: models.UserLoign{
-// 					Email:    "gokul@gmail.com",
-// 					PassWord: "hffj",
-// 				},
-// 			},
-
-// 			StubDetails: func(userRepo mocks.MockUserRepository, helper mocks.MockHelper, loginData args) {
-// 				gomock.InOrder(
-// 					userRepo.EXPECT().CheckUserAvailability(loginData.user.Email).Times(1).Return(true),
-// 					userRepo.EXPECT().UserBlockStatus(loginData.user.Email).Times(1).Return(false, nil),
-// 					userRepo.EXPECT().FindUserByEmail(loginData.user).Times(1).Return(
-// 						models.UserSignInResponse{
-// 							ID:       1,
-// 							Email:    loginData.user.Email,
-// 							Password: "$2a$10$kgGwgsjgQ.ycqVn.BHRwT.FvW57fvV8ncmtYcmWTNMsLvStW4EvQi", // Replace with the hashed password
-// 						}, nil),
-// 					helper.EXPECT().GenerateTokenClients(models.UserDeatilsResponse{
-// 						ID:       1,
-// 						Email:    loginData.user.Email,
-// 						Password: "$2a$10$kgGwgsjgQ.ycqVn.BHRwT.FvW57fvV8ncmtYcmWTNMsLvStW4EvQi", // Replace with the hashed password
-// 					}).Times(1).Return("ajjsjsjsjsjs.sjsjsjsjsjs.sjsjsjsjs", nil),
-// 				)
-// 			},
-
-// 			expectedOutput: models.TokenUsers{
-// 				Users: models.UserDeatilsResponse{
-// 					ID:       1,
-// 					Email:    "gokul@gmail.com",
-// 					Password: "", // Password should not be exposed in the response
-// 				},
-// 				Token: "ajjsjsjsjsjs.sjsjsjsjsjs.sjsjsjsjs",
-// 			},
-// 			expectedError: nil,
-// 		},
-// 		"user not found": {
-// 			input: args{
-// 				user: models.UserLoign{
-// 					Email:    "nonexistent@gmail.com",
-// 					PassWord: "hffj",
-// 				},
-// 			},
-
-// 			StubDetails: func(userRepo mocks.MockUserRepository, helper mocks.MockHelper, loginData args) {
-// 				userRepo.EXPECT().CheckUserAvailability(loginData.user.Email).Times(1).Return(false)
-// 			},
-
-// 			expectedOutput: models.TokenUsers{},
-// 			expectedError:  ErrUserNotFound,
-// 		},
-// 		"user blocked by admin": {
-// 			input: args{
-// 				user: models.UserLoign{
-// 					Email:    "blocked@gmail.com",
-// 					PassWord: "hffj",
-// 				},
-// 			},
-
-// 			StubDetails: func(userRepo mocks.MockUserRepository, helper mocks.MockHelper, loginData args) {
-// 				gomock.InOrder(
-// 					userRepo.EXPECT().CheckUserAvailability(loginData.user.Email).Times(1).Return(true),
-// 					userRepo.EXPECT().UserBlockStatus(loginData.user.Email).Times(1).Return(true, nil),
-// 				)
-// 			},
-
-// 			expectedOutput: models.TokenUsers{},
-// 			expectedError:  ErrBlockedByAdmin,
-// 		},
-// 		"incorrect password": {
-// 			input: args{
-// 				user: models.UserLoign{
-// 					Email:    "gokul@gmail.com",
-// 					PassWord: "wrongpassword",
-// 				},
-// 			},
-
-// 			StubDetails: func(userRepo mocks.MockUserRepository, helper mocks.MockHelper, loginData args) {
-// 				gomock.InOrder(
-// 					userRepo.EXPECT().CheckUserAvailability(loginData.user.Email).Times(1).Return(true),
-// 					userRepo.EXPECT().UserBlockStatus(loginData.user.Email).Times(1).Return(false, nil),
-// 					userRepo.EXPECT().FindUserByEmail(loginData.user).Times(1).Return(
-// 						models.UserSignInResponse{
-// 							ID:       1,
-// 							Email:    loginData.user.Email,
-// 							Password: "$2a$10$kgGwgsjgQ.ycqVn.BHRwT.FvW57fvV8ncmtYcmWTNMsLvStW4EvQi", // Replace with the hashed password
-// 						}, nil),
-// 					helper.EXPECT().GenerateTokenClients(gomock.Any()).Times(0),
-// 				)
-// 			},
-
-// 			expectedOutput: models.TokenUsers{},
-// 			expectedError:  ErrIncorrectPassword,
-// 		},
-// 	}
-
-// 	for _, test := range testData {
-// 		test.StubDetails(*userRepo, *helper, test.input)
-
-// 		tokenusers, err := userUseCase.LoginHandler(test.input.user)
-// 		assert.Equal(t, test.expectedOutput.Users.ID, tokenusers.Users.ID)
-// 		assert.Equal(t, test.expectedOutput.Users.Email, tokenusers.Users.Email)
-// 		assert.Equal(t, "", tokenusers.Users.Phone) // Ensure the password is not exposed in the response
-// 		assert.Equal(t, test.expectedOutput.Token, tokenusers.Token)
-// 		assert.Equal(t, test.expectedError, err)
-// 	}
-// 	cntrl.Finish()
-// }
 
 func Test_User_UserSignUp(t *testing.T) {
 	ctrl := gomock.NewController(t)
@@ -240,42 +23,34 @@ func Test_User_UserSignUp(t *testing.T) {
 	helper := mock.NewMockHelper(ctrl)
 	cfg := config.Config{}
 
-	type args struct {
-		user models.UserDetails
-		ref  string
-	}
-
 	userUseCase := NewUserUseCase(userRepo, cfg, otpRepo, helper, orderRepo)
 
 	testData := map[string]struct {
-		input          args
-		StubDetails    func(mock.MockUserRepository, mock.MockHelper, args)
+		input          models.UserDetails
+		stubDetails    func(mock.MockUserRepository, mock.MockOrderRepository, models.UserDetails, mock.MockHelper)
 		expectedOutput models.TokenUsers
 		expectedError  error
 	}{
 		"success": {
-			input: args{
-				user: models.UserDetails{
-					Name:            "gokul",
-					Email:           "gokul@gmail.com",
-					Phone:           "6282288363",
-					Password:        "hffj",
-					ConfirmPassWord: "hffj",
-				},
-				ref: "gok",
+			input: models.UserDetails{
+				Name:            "gokul",
+				Email:           "gokul@gmail.com",
+				Phone:           "6282288363",
+				Password:        gomock.Any().String(),
+				ConfirmPassWord: gomock.Any().String(),
 			},
-			StubDetails: func(userRepo mock.MockUserRepository, helper mock.MockHelper, arg args) {
+			stubDetails: func(userRepo mock.MockUserRepository, orderRepo mock.MockOrderRepository, signupData models.UserDetails, helper mock.MockHelper) {
 				gomock.InOrder(
-					userRepo.EXPECT().CheckUserAvailability(arg.user.Email).Times(1).Return(false),
-					userRepo.EXPECT().FindUserFromReference("1234").Times(1).Return(false),
-					helper.EXPECT().PasswordHashing(arg.user.Password).Times(1).Return(gomock.Any().String(), nil),
+					userRepo.EXPECT().CheckUserAvailability(signupData.Email).Times(1).Return(false),
+					userRepo.EXPECT().FindUserFromReference("1234").Times(1).Return(1, nil),
+					helper.EXPECT().PasswordHashing(signupData.Password).Times(1).Return(gomock.Any().String(), nil),
 					helper.EXPECT().GenerateRefferalCode().Times(1).Return(gomock.Any().String(), nil),
-					userRepo.EXPECT().UserSignUp(arg.user, gomock.Any().String()).Times(1).Return(
+					userRepo.EXPECT().UserSignUp(signupData, gomock.Any().String()).Times(1).Return(
 						models.UserDeatilsResponse{
 							Id:    1,
-							Name:  arg.user.Name,
-							Email: arg.user.Email,
-							Phone: arg.user.Phone,
+							Name:  signupData.Name,
+							Email: signupData.Email,
+							Phone: signupData.Phone,
 						}, nil,
 					),
 					helper.EXPECT().GenerateTokenClients(models.UserDeatilsResponse{
@@ -284,6 +59,8 @@ func Test_User_UserSignUp(t *testing.T) {
 						Email: "gokul@gmail.com",
 						Phone: "6282288363",
 					}).Times(1).Return(gomock.Any().String(), nil),
+					userRepo.EXPECT().CreditReferencePointsToWallet(1).Times(1).Return(nil),
+					orderRepo.EXPECT().CreateNewWallet(1).Times(1).Return(1, nil),
 				)
 			},
 			expectedOutput: models.TokenUsers{
@@ -293,20 +70,54 @@ func Test_User_UserSignUp(t *testing.T) {
 					Email: "gokul@gmail.com",
 					Phone: "6282288363",
 				},
-				Token: "ajjsjsjsjsjs.sjsjsjsjsjs.sjsjsjsjs",
+				Token: "abcdefghijklmop",
 			},
+
 			expectedError: nil,
 		},
-		"user already exist":{
+		"user already exist": {
 			input: models.UserDetails{
-				Name: "gokul",
-				Email: "gokul@gmail.com",
-				Phone: "6282288363",
-				Password: gomock.Any().String(),
-				ConfirmPassWord: "gokk",
+				Name:            "gokul",
+				Email:           "gokul@gmail.com",
+				Phone:           "6282288363",
+				Password:        gomock.Any().String(),
+				ConfirmPassWord: gomock.Any().String(),
 			},
-			StubDetails: func(userRepo mock.MockUserRepository, helper mock.MockHelper, arg args) {},
-		}
+			stubDetails: func(userRepo mock.MockUserRepository, orderRepo mock.MockOrderRepository, signupData models.UserDetails, helper mock.MockHelper) {
+				gomock.InOrder(
+					userRepo.EXPECT().CheckUserAvailability(signupData.Email).Times(1).Return(true),
+				)
+			},
+			expectedOutput: models.TokenUsers{},
+			expectedError:  errors.New("user already exist,sign in"),
+		},
+		"password missmatch": {
+			input: models.UserDetails{
+				Name:            "gokul",
+				Email:           "gokul@gmail.com",
+				Phone:           "6282288363",
+				Password:        "goku",
+				ConfirmPassWord: "guku",
+			},
+			stubDetails: func(userRepo mock.MockUserRepository, orderRepo mock.MockOrderRepository, signupData models.UserDetails, helper mock.MockHelper) {
+				gomock.InOrder(
+					userRepo.EXPECT().CheckUserAvailability(signupData.Email).Times(1).Return(false),
+				)
+			},
+			expectedOutput: models.TokenUsers{},
+			expectedError:  errors.New("password dosnt match"),
+		},
+	}
+	for name, test := range testData {
+		fmt.Println("name", name)
+		test.stubDetails(*userRepo, *orderRepo, test.input, *helper)
+		tokenUsers, err := userUseCase.UserSignUp(test.input, "1234")
+
+		assert.Equal(t, test.expectedOutput.Users.Id, tokenUsers.Users.Id)
+		assert.Equal(t, test.expectedOutput.Users.Name, tokenUsers.Users.Name)
+		assert.Equal(t, test.expectedOutput.Users.Email, tokenUsers.Users.Email)
+		assert.Equal(t, test.expectedError, err)
+
 	}
 
 }
